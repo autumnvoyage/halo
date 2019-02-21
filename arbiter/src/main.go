37146c433421771a24e720f85b5b67ab06d61023
Alexander Nicholi <me@nicholatian.com>
2019-02-21 14:07:58 -0500

    map handshake structs
    fix listening address clopts
    
    gofmt and some logging fixes

--- arbiter/src/main.go
+++ arbiter/src/main.go
@@ -11,22 +11,54 @@ import (
 
 var (
 	upgrader = websocket.Upgrader{
-		ReadBufferSize: 1024,
+		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
 	}
 	addr = flag.String("addr", "127.0.0.1:1941", "HTTP service address")
 )
 
 const (
-	writeWait = 10 * time.Second
-	maxMsgSize = 8192
-	pongWait = 60 * time.Second
-	pingPeriod = (pongWait * 9) / 10
+	writeWait        = 10 * time.Second
+	maxMsgSize       = 8192
+	pongWait         = 60 * time.Second
+	pingPeriod       = (pongWait * 9) / 10
 	closeGracePeriod = 10 * time.Second
+	goodtext = [64]byte{
+		105, 102, 32, 105, 32, 104, 97, 100, 32, 97, 32, 109, 105, 108,
+		108, 105, 111, 110, 32, 98, 117, 99, 107, 115, 32, 105, 32,
+		119, 111, 117, 108, 100, 32, 98, 32, 117, 121, 32, 97, 32, 80,
+		114, 101, 103, 110, 97, 110, 116, 32, 115, 101, 97, 114, 115,
+		32, 109, 97, 110, 110, 101, 113, 117, 105, 110
+	}
 )
 
 func handleMsg(data []byte) {
 
+}
+
+// For messages, we assume we have TLS
+
+// incoming, RSA enc, server’s key (they should have done a lookup)
+type HandshakeIn struct {
+	Magic [4]byte // ASCII no-NUL "HMSG"
+	Version uint32 // 0
+	PubkeyFprint [20]byte // Lookup the client’s PGP
+	Goodtext     [64]byte // Good text constant, see above
+}
+
+// outgoing, RSA enc, client’s key
+type HandshakeOut struct {
+	Magic [4]byte // ASCII no-NUL "HMSG"
+	Version uint32 // 0
+	TTL uint32 // Lifetime of session
+	SessKey [32]byte // AES-256, future msgs use this instead of RSA
+	SessId uint64 // Tag future comms
+}
+
+type MessageIn struct {
+	Magic [4]byte // ASCII no-NUL "EMSG"
+	SessId uint64
+	Payload []byte // decrypted with AES-256 sesskey
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -35,13 +67,11 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatalf("Failed to upgrade HTTP to WebSocket: %v", err)
 		return
 	}
+	log.Println("Upgraded HTTP to WebSocket.")
 	for {
-		msgType, data, err := conn.ReadMessage()
+		msgType, data, err := conn.ReadJSON()
 		if err != nil {
-			log.Fatalf("ReadMessage() failed: %v", err)
-			return
-		}
-		if msgType == websocket.BinMessage {
+			log.Fatalf("Conn.ReadJSON() failed: %v", err)
 			return
 		}
 		handleMsg(data)
@@ -50,13 +80,12 @@ func wsHandler(w http.ResponseWriter, r *http.Request) {
 
 func httpSetup(addr string) {
 	http.HandleFunc("/", wsHandler)
+	log.Println("Set HTTP server handler")
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
 func main() {
 	flag.Parse()
-	if len(flag.Args()) < 1 {
-		log.Fatal("Must specify listening address")
-	}
+	log.Printf("Service address: %v", *addr)
 	httpSetup(*addr)
 }
